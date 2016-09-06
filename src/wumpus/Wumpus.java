f6a97dff6343adc01fa4acc0fe3f475fc4a43924
Rodrigo Silva Lima <rodrig0_lima@hotmail.com>
2016-09-06 14:37:46 -0300

    Resolução de conflitos
    Resolução de conflitos

--- src/wumpus/Wumpus.java
+++ src/wumpus/Wumpus.java
@@ -1,67 +1,3 @@
-<<<<<<< HEAD:src/wumpus/Wumpus.java
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package wumpus;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import wumpus.ambiente.Ambiente;
-
-/**
- *
- * @author Fabio
- */
-public class Wumpus extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Iniciar");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                Ambiente amb = new Ambiente(0,0,0,0);
-                amb.getPercepcao();
-                amb.avancar();
-                amb.getPercepcao();
-                amb.girar(0);
-                amb.avancar();
-                amb.getPercepcao();
-                amb.girar(1);
-                amb.girar(1);
-                amb.avancar();
-                amb.getPercepcao();
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Mundo do Wumpus!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -143,4 +79,3 @@ public class Wumpus extends Application {
     }
     
 }
->>>>>>> origin/master:Wumpus/src/wumpus/Wumpus.java
--- src/wumpus/agente/Agente.java
+++ src/wumpus/agente/Agente.java
@@ -1,74 +1,3 @@
-<<<<<<< HEAD:src/wumpus/agente/Agente.java
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package wumpus.agente;
-import java.util.ArrayList;
-import wumpus.ambiente.Ambiente;
-
-/**
- *
- * @author Rodrigo
- */
-public class Agente {
-    // Constantes do agente
-    private final int DIRECAO_CIMA = 0;
-    private final int DIRECAO_DIREITA = 1;
-    private final int DIRECAO_BAIXO = 2;
-    private final int DIRECAO_ESQUERDA = 3;
-    private final int SENTIDO_ESQUERDA = 0;
-    private final int SENTIDO_DIREITA = 1;
-    // Atributos do agente
-    private Ambiente ambiente;
-    private int direcaoAtual;
-    private ArrayList<Integer> direcoesVolta;
-    private ArrayList<Casa> visitadas;
-    private ArrayList<Casa> possiveis;
-    private boolean possuiOuro;
-    private Casa posicaoAtual;
-    
-    public Agente(Ambiente ambiente){
-        this.ambiente = ambiente;
-        this.direcaoAtual = DIRECAO_CIMA;
-        this.possuiOuro = false;
-        int[] x = {0,0};
-        this.posicaoAtual = new Casa(x,ambiente.getPercepcao());
-        posicaoAtual.setOk();
-        posicaoAtual.setVisitada();
-        direcoesVolta = new ArrayList<>();
-        possiveis = new ArrayList<>();
-        visitadas = new ArrayList<>();
-        visitadas.add(posicaoAtual);
-    }
-    
-    public void voltaComeco(){
-        
-    }
-    
-    public boolean matarWumpus(){
-        return ambiente.atirar();
-    }
-    
-    public boolean pegarOuro(){
-        return ambiente.pegar();
-    }
-    
-    public ArrayList<Casa> buscarCasasPossiveis(){
-        return null;
-    }
-    
-    public boolean irParaCasa(int x,int y){
-        return false;
-    }
-    
-    public void iniciar(){
-        
-    }
-}
-=======
-   
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -361,4 +290,3 @@ public class Agente {
       }
     }
 }
->>>>>>> origin/master:Wumpus/src/wumpus/agente/Agente.java
